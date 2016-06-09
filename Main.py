12db71581968f56d00dcd1d8045c2a533122335f
sh <bedrook@gmail.com>
2016-06-09 16:12:38 +0900

    Test

---
+++ Main.py
@@ -0,0 +1,71 @@
+__author__ = 'sh'
+import pygame
+import os
+import sys
+import time
+import GameInfo
+import Ball
+
+pygame.init()
+
+screen = pygame.display.set_mode((GameInfo.displayWidth,GameInfo.displayHeight))
+
+clock = pygame.time.Clock()
+
+key = 0
+text =" "
+
+ball = Ball.Ball(170,0)
+
+while GameInfo.Run:
+
+    #KEY INPUT
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameInfo.Run=False
+        elif event.type == pygame.KEYDOWN:
+            keyStatus ="KEY DOWN"
+            key = event.key
+
+        elif event.type == pygame.KEYUP:
+            keyStatus ="KEY UP"
+            key = event.key
+
+    #UPDATE
+    if key == pygame.K_RIGHT and keyStatus == "KEY DOWN":
+            ball.setStatus(newxspeed=ball.xspeed+2)
+    if key == pygame.K_LEFT and keyStatus == "KEY DOWN":
+            ball.setStatus(newxspeed=ball.xspeed-2)
+
+    if key == pygame.K_SPACE and keyStatus =="KEY DOWN":
+            ball.setStatus(newyspeed=ball.yspeed-10)
+
+    if ball.y+50>=GameInfo.displayHeight:
+        ball.setStatus(newyspeed=-15)
+
+    if ball.x <= 0 :
+        ball.setStatus(newxspeed=ball.xspeed*-0.9)
+        ball.setStatus(newx=0)
+
+    elif ball.x + 50 >= GameInfo.displayWidth :
+        ball.setStatus(newxspeed=ball.xspeed*-0.9)
+        ball.setStatus(newx=GameInfo.displayWidth-ball.radius)
+
+    #DRAW
+    screen.fill((255,255,255))
+
+    pygame.draw.ellipse(screen,(0,0,0),pygame.Rect([ball.x,ball.y],(ball.radius,ball.radius)))
+
+    ball.update()
+
+    if key :
+        sf = pygame.font.SysFont("Monospace",20,bold=True)
+        textStr = pygame.key.name(key)+" "+keyStatus+" "+str(ball.y)
+        text = sf.render(textStr,True,(0,172,255))
+        screen.blit(text,(50,40))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+
+pygame.QUIT()
