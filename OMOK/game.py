986e1ee7ca7d5bad7781a1d09391ff1b9db93f88
cube <bedrook@gmail.com>
2018-05-21 13:28:46 +0900

    add OMOK

--- OMOK/game.py
+++ OMOK/game.py
@@ -11,16 +11,19 @@ class STONE(Enum):
   BLACK = auto()
   WHITE = auto()
 
+WIN_STONE_COUNT = 5
+
 print(list(STONE))
 
 screen = pygame.display.set_mode((1000, 1000))
 
 clock = pygame.time.Clock()
 
-run = True
+
 
 class OMOK:
   def __init__(self, width, height, mode):
+    self.run = True
     self.width = width
     self.height = height
     self.mode = mode
@@ -43,17 +46,93 @@ class OMOK:
           if height * j - halfh < pos[1] < height * (j + 1) - halfh:
             return (i, j)
     return False
+
+  def stoneCount(self, i, j, stone):
+    
+    print(i, j, stone)
+    #오른쪽
+    if j + WIN_STONE_COUNT < self.width + 1:
+      count = 0
+      for k in range(WIN_STONE_COUNT):
+        if self.map[i][j + k] == stone:
+          count += 1
+          if count == WIN_STONE_COUNT:
+            return stone
+        else:
+          break
+
+    #좌하단
+    if i + WIN_STONE_COUNT < self.height + 1 and j - WIN_STONE_COUNT >= 0:
+      count = 0
+      for k in range(WIN_STONE_COUNT):
+        if self.map[i + k][j - k] == stone:
+          count += 1 
+          if count == WIN_STONE_COUNT:
+            return stone
+        else:
+          break
+    #아래
+    if i + WIN_STONE_COUNT < self.height + 1:
+      count = 0
+      for k in range(WIN_STONE_COUNT):
+        if self.map[i + k][j] == stone:
+          count += 1
+          if count == WIN_STONE_COUNT:
+            return stone
+        else:
+          break
+    #우하단
+    if i + WIN_STONE_COUNT < self.height + 1 and j + WIN_STONE_COUNT < self.width + 1:
+      count = 0
+      for k in range(WIN_STONE_COUNT):
+        if self.map[i + k][j + k] == stone:
+          count += 1
+          if count == WIN_STONE_COUNT:
+            return stone
+        else:
+          break
+
+    return STONE.NONE
+
+  def gameEndCheck(self):
+    stone = STONE.NONE
   
+    for i in range(self.width + 1):
+      for j in range(self.height + 1):
+        if self.map[i][j] != STONE.NONE:
+          stone = self.stoneCount(i, j, self.map[i][j])
+          if stone != STONE.NONE:
+            return stone
+    
+    return stone
+          
+          
+        
   def proc(self, pos):
     print(pos)
     idx = self.posToStoneIdx(pos)
+
+    if self.map[idx[0]][idx[1]] != STONE.NONE:
+      return
+
+      
+
     if idx != False:
       print("DEBUG : ", idx)
       self.map[idx[0]][idx[1]] = self.turn
+
+      stone = self.gameEndCheck()
+      if stone != STONE.NONE:
+        print("WIN : ", stone)
+        self.run = False
+        return
+
       if self.turn == STONE.BLACK:
         self.turn = STONE.WHITE
       else:
         self.turn = STONE.BLACK
+
+    
 
   def draw(self):
 
@@ -70,7 +149,7 @@ class OMOK:
       pygame.draw.line(screen, pygame.color.Color(0, 0, 0), 
       (width * j, 0), 
       (width * j, screen.get_height()), 3)
-    
+
     for i in range(self.width + 1):
       for j in range(self.height + 1):
         if self.map[i][j] == STONE.BLACK:
@@ -80,18 +159,40 @@ class OMOK:
           pygame.draw.ellipse(screen, (0xff,0xff,0xff), pygame.Rect([width * i - self.stoneRadius, height * j - self.stoneRadius], (self.stoneRadius * 2, self.stoneRadius * 2)))
           pygame.draw.ellipse(screen, (0,0,0), pygame.Rect([width * i - self.stoneRadius, height * j - self.stoneRadius], (self.stoneRadius * 2, self.stoneRadius * 2)), 2)
 
+  def gameEnd(self):
+
+    if self.turn == STONE.BLACK:
+      screen.fill((0,0,0))
+    else:
+      screen.fill((0xff, 0xff, 0xff))
+    sf = pygame.font.SysFont("Monospace",40,bold=True)
+    textStr = "WIN"
+    text = sf.render(textStr,True,(0,172,255))
+    screen.blit(text,((screen.get_width()/2, screen.get_height()/2)))
+    if self.turn == STONE.BLACK:
+      textStr = "BLACK"
+    else:
+      textStr = "WHITE"
+    text = sf.render(textStr,True,(0,172,255))
+    screen.blit(text,(screen.get_width()/2, screen.get_height()/2  + 100))
+
+
+    pygame.display.flip()
+    time.sleep(2)
+
+
 
 
 omok = OMOK(19, 19, 0)
 
-while run:
+while omok.run:
   key = 1
   pos = 0
   #KEY INPUT
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      print("GAME END")
-      run = False
+
+      omok.run = False
       break
     elif event.type == pygame.KEYDOWN:
       keyStatus = 1
@@ -119,3 +220,4 @@ while run:
   pygame.display.flip()
   clock.tick(10)
 
+omok.gameEnd()
